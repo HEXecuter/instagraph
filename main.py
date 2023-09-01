8e071cbafce2725414e47fa9abf5eda42a13670f
BuildTools <ealvarez2025@gmail.com>
2023-09-01 15:16:39 -0700

    Fix bugs with SQL statements and pull aggregated data from SQLite

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from tkinter import filedialog
 from tkinter import Tk
 import datetime
 
-def isValid(message: dict):
+
+def is_valid(message: dict):
     if "content" not in message:
         return False
 
@@ -20,12 +21,9 @@ def isValid(message: dict):
     return True
 
 
-# Liked a message
-# Reacted \u00f0\u009f\u0098\u00ad to your message
-
 cur = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES).cursor()
 cur.execute("CREATE TABLE messages("
-            "name TEXT PRIMARY KEY,"
+            "name TEXT,"
             "date DATE,"
             "count INTEGER)"
             )
@@ -48,9 +46,12 @@ for folder_name in os.listdir(folder_selected):
             continue
 
         for message in conversation["messages"]:
-            if isValid(message):
-                cur.execute("INSERT INTO messages(?, ?, ?)", (
+            if is_valid(message):
+                cur.execute("INSERT INTO messages VALUES(?, ?, ?)", (
                     folder_name,
                     datetime.date.fromtimestamp(message["timestamp_ms"] / 1000),
                     1
                 ))
+
+for aggregate_conversations in cur.execute("SELECT name, date, SUM(count) FROM messages GROUP BY name, date").fetchall():
+    min_date, max_date = cur.execute("SELECT MIN(date), MAX(date) from messages WHERE name = ? GROUP BY name", (aggregate_conversations[0],)).fetchall()[0]
